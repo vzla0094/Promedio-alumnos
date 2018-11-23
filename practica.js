b5238018ec10d911e6b77bdfbaf5b7c5d6469c1a
vzla0094 <43974195+vzla0094@users.noreply.github.com>
2018-11-22 20:48:07 -0700

    Add files via upload

---
+++ practica.js
@@ -0,0 +1,26 @@
+
+
+
+let students = []; //store info
+
+//Push new objects into array
+function pusher(nameCall,gradesCall) {
+  return students.push({name:nameCall,grades:gradesCall})
+}
+
+function getAverage(nameToLook) {
+  for (let index = 0; index < students.length; index++) {
+    if(students[index].name===nameToLook){
+      let average = students[index].grades.reduce((accumulator, current)=>accumulator+current)/students[index].grades.length;
+      if (average<7){
+        return `Reprobó con ${average} por burro`
+      }else{
+        return average;
+      }
+    }    
+  }
+}
+
+let test1 = document.getElementById("name").value;
+let test2 = 0;
+console.log(test1);
